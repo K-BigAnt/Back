0a263a4081610e02241c2e9688f233c8d9e1f685
seongjunkim <tjdwns5063@naver.com>
2024-07-30 17:41:57 +0900

    Dto 및 Entity 구현

---
+++ src/main/java/com/bigant/gaeme/repository/entity/KrStock.java
@@ -0,0 +1,31 @@
+package com.bigant.gaeme.repository.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class KrStock extends Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private boolean isDelisting;
+
+    private String isinCode;
+
+}
