056ea0b4695c070336ee7383710f63023344cea9
seongjunkim <tjdwns5063@naver.com>
2024-07-31 17:31:44 +0900

    feat: 국내 주식을 조회한 결과를 DB에 저장하는 기능 구현

--- src/main/java/com/bigant/gaeme/dao/StockDao.java
+++ src/main/java/com/bigant/gaeme/dao/StockDao.java
@@ -1,12 +1,11 @@
 package com.bigant.gaeme.dao;
 
 import com.bigant.gaeme.dao.dto.KrStockDto;
-import java.net.URISyntaxException;
 import java.util.List;
 
 public interface StockDao {
 
-    List<KrStockDto> getStock() throws URISyntaxException;
+    List<KrStockDto> getStock();
 
     List<KrStockDto> getEtf();
 
