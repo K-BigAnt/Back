25af4724481dd27aafeefb341a4f331f07622088
seongjunkim <tjdwns5063@naver.com>
2024-08-02 16:41:01 +0900

    feat: UsStock 구현

---
+++ src/main/java/com/bigant/gaeme/repository/entity/UsStock.java
@@ -0,0 +1,29 @@
+package com.bigant.gaeme.repository.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class UsStock extends Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String country;
+
+}
