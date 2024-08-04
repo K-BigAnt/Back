23397e0430350b8c127aa406c537f5355b550024
seongjunkim <tjdwns5063@naver.com>
2024-08-04 16:28:29 +0900

    refactor: 성능 개선
    두개의 리스트의 요소를 비교하던 로직 (n*n)을 Map을 사용하도록 해서 최적화.

--- src/main/java/com/bigant/gaeme/usecase/StockDataUsecase.java
+++ src/main/java/com/bigant/gaeme/usecase/StockDataUsecase.java
@@ -2,6 +2,9 @@ package com.bigant.gaeme.usecase;
 
 import com.bigant.gaeme.repository.entity.Stock;
 import com.bigant.gaeme.repository.enums.StockType;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -15,12 +18,21 @@ public class StockDataUsecase<T extends Stock, R extends JpaRepository<T, Long>>
     private final R stockRepository;
 
     public void saveStock(List<T> newStocks, StockType type) {
-        Set<T> exists = stockRepository.findAll().stream()
+        Map<String, T> exists = stockRepository.findAll().stream()
                 .filter(stock -> stock.getType() == type)
-                .collect(Collectors.toSet());
+                .collect(Collectors.toMap(Stock::getSymbol, item -> item));
 
-        List<T> addStocks = newStocks.stream().filter(item -> exists.stream().noneMatch(item2 -> item.getSymbol().equals(item2.getSymbol()))).toList();
-        List<T> delistingStocks = exists.stream().filter(item -> newStocks.stream().anyMatch(item2 -> item.getSymbol().equals(item2.getSymbol()))).toList();
+        Map<String, T> newStocksBySymbol = newStocks.stream()
+                .collect(Collectors.toMap(Stock::getSymbol, item -> item));
+
+        List<T> addStocks = newStocksBySymbol.entrySet().stream()
+                .filter(item -> !exists.containsKey(item.getKey()))
+                .map(Entry::getValue)
+                .toList();
+        List<T> delistingStocks = exists.entrySet().stream()
+                .filter(item -> newStocksBySymbol.containsKey(item.getKey()))
+                .map(Entry::getValue)
+                .toList();
 
         stockRepository.saveAll(addStocks);
         delistStocks(delistingStocks);
