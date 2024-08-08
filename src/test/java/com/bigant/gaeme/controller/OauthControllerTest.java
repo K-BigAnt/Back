795011cab51a1ad6080d00b2ac1415556592784a
tjdwns5063 <73011361+tjdwns5063@users.noreply.github.com>
2024-08-08 23:05:36 +0900

    feat: 회원가입&로그인 API 테스트 구현

--- src/test/java/com/bigant/gaeme/controller/OauthControllerTest.java
+++ src/test/java/com/bigant/gaeme/controller/OauthControllerTest.java
@@ -8,10 +8,13 @@ import static org.springframework.restdocs.request.RequestDocumentation.paramete
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.bigant.gaeme.repository.enums.AuthCorp;
+import com.bigant.gaeme.service.AuthResponseDto;
 import com.bigant.gaeme.service.OauthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
@@ -23,12 +26,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultHandler;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(OauthController.class)
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 @AutoConfigureRestDocs(uriPort = 80)
 @ExtendWith(RestDocumentationExtension.class)
 public class OauthControllerTest {
@@ -39,13 +42,15 @@ public class OauthControllerTest {
     @MockBean
     private OauthService oauthService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    @WithMockUser
     public void getAuthCodeTest() throws Exception {
         BDDMockito.given(oauthService.getAuthUri("http://localhost/auth/callback", AuthCorp.KAKAO))
                 .willReturn("http://localhost/oauth/authorize?client_id=kakao_id&response_type=code&redirect_uri=http://localhost/auth/callback");
 
-        mockMvc.perform(RestDocumentationRequestBuilders.get("http://localhost/v1/auth/{auth_corp}", "kakao")
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/v1/auth/{auth_corp}", "kakao")
                         .param("redirect_uri", "http://localhost/auth/callback"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(MockMvcResultMatchers.redirectedUrl(
@@ -64,6 +69,40 @@ public class OauthControllerTest {
                 ),
                 queryParameters(
                         parameterWithName("redirect_uri").description("인가코드를 받을 리다이렉트 URI")
+                )
+        );
+    }
+
+    @Test
+    public void signInOrSignUpTest() throws Exception {
+        AuthResponseDto result = AuthResponseDto.builder()
+                .token("jwt token")
+                .isSignIn(true)
+                .build();
+        BDDMockito.given(oauthService.signInOrSignUp(BDDMockito.any(), BDDMockito.any(), BDDMockito.any()))
+                .willReturn(result);
+
+        mockMvc.perform(RestDocumentationRequestBuilders.post("/v1/auth/{auth_corp}", "naver")
+                        .queryParam("code", "auth_code_blahblah")
+                        .queryParam("redirect_uri", "http://localhost/auth/callback"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(result)))
+                .andDo(getSignInOrSignUpPostResultHandler())
+                .andDo(print());
+
+        BDDMockito.verify(oauthService).signInOrSignUp("http://localhost/auth/callback", "auth_code_blahblah", AuthCorp.NAVER);
+    }
+
+    private ResultHandler getSignInOrSignUpPostResultHandler() {
+        return document("sign-in-or-sign-up/post",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                queryParameters(
+                        parameterWithName("code").description("인가 코드"),
+                        parameterWithName("redirect_uri").description("인가코드를 받은 리다이렉트 URI")
+                ),
+                pathParameters(
+                        parameterWithName("auth_corp").description("OAuth 플랫폼 (kakao, naver, google)")
                 )
         );
     }
