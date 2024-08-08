1280a2f4f22be36e7303e622acf8e46313f951eb
tjdwns5063 <73011361+tjdwns5063@users.noreply.github.com>
2024-08-08 23:21:45 +0900

    fix: rebase 시 충돌 잘못 병합한 것 수정

--- src/main/java/com/bigant/gaeme/config/GaemeConfig.java
+++ src/main/java/com/bigant/gaeme/config/GaemeConfig.java
@@ -1,14 +1,13 @@
 package com.bigant.gaeme.config;
 
-import com.bigant.gaeme.dao.KrStockDao;
-import com.bigant.gaeme.dao.UsStockDao;
+import com.bigant.gaeme.dao.OauthClient;
 import com.bigant.gaeme.repository.KrStockRepository;
 import com.bigant.gaeme.repository.UsStockRepository;
 import com.bigant.gaeme.repository.entity.KrStock;
 import com.bigant.gaeme.repository.entity.UsStock;
 import com.bigant.gaeme.usecase.StockDataUsecase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,6 +27,50 @@ public class GaemeConfig {
     @Bean
     public StockDataUsecase<KrStock, KrStockRepository> krStockDataUsecase() {
         return new StockDataUsecase<>(krStockRepository);
+    }
+
+    @Bean
+    public OauthClient kakaoOauthClient(
+            @Value("${oauth.kakao.base_url}") String baseUrl,
+            @Value("${oauth.kakao.client_id}") String clientId,
+            @Value("${oauth.kakao.auth_path}") String authPath
+    ) {
+        return new OauthClient(
+                baseUrl,
+                clientId,
+                authPath,
+                null
+        );
+    }
+
+    @Bean
+    public OauthClient naverOauthClient(
+            @Value("${oauth.naver.base_url}") String baseUrl,
+            @Value("${oauth.naver.client_id}") String clientId,
+            @Value("${oauth.naver.auth_path}") String authPath
+    ) {
+        return new OauthClient(
+                baseUrl,
+                clientId,
+                authPath,
+                null
+        );
+    }
+
+    @Bean
+    public OauthClient googleOauthClient(
+            @Value("${oauth.google.base_url}") String baseUrl,
+            @Value("${oauth.google.client_id}") String clientId,
+            @Value("${oauth.google.auth_path}") String authPath,
+            @Value("${oauth.google.scope}") String scope
+
+    ) {
+        return new OauthClient(
+                baseUrl,
+                clientId,
+                authPath,
+                scope
+        );
     }
 
 }
