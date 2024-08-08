5ad93f800588317390ba20daf94025eb39ddc9a7
tjdwns5063 <73011361+tjdwns5063@users.noreply.github.com>
2024-08-08 21:56:43 +0900

    feat: 유저 정보 파싱하는 부분 수정

--- src/main/java/com/bigant/gaeme/config/GaemeConfig.java
+++ src/main/java/com/bigant/gaeme/config/GaemeConfig.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Clock;
+
 @Configuration
 @RequiredArgsConstructor
 public class GaemeConfig {
@@ -83,6 +85,11 @@ public class GaemeConfig {
                 tokenUrl,
                 infoUrl
         );
+    }
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemUTC();
     }
 
 }
