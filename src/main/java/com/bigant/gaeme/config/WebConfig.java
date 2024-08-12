b75fc823837dec3736bdf2372fafa9f015f54653
seongjunkim <tjdwns5063@naver.com>
2024-08-12 16:54:38 +0900

    fix: checkstyle

--- src/main/java/com/bigant/gaeme/config/WebConfig.java
+++ src/main/java/com/bigant/gaeme/config/WebConfig.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
