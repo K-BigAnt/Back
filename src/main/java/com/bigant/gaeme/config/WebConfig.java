3abfc72f8e41f87c51c7e8ab029b1552ef8039cd
Seongjki <73011361+tjdwns5063@users.noreply.github.com>
2024-08-12 16:48:49 +0900

    chore: cors 설정 추가 (#36)

--- src/main/java/com/bigant/gaeme/config/WebConfig.java
+++ src/main/java/com/bigant/gaeme/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.bigant.gaeme.repository.enums.AuthCorp;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -21,6 +22,12 @@ public class WebConfig implements WebMvcConfigurer {
             return AuthCorp.valueOf(source.toUpperCase());
         }
 
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
     }
 
 }
