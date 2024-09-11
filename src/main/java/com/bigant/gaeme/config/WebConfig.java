60a51ce310dab35a59afbf966794791948b80541
Seongjki <73011361+tjdwns5063@users.noreply.github.com>
2024-09-11 21:23:26 +0900

    fix: 토큰 쿠키에 추가 (#50)

--- src/main/java/com/bigant/gaeme/config/WebConfig.java
+++ src/main/java/com/bigant/gaeme/config/WebConfig.java
@@ -23,6 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
