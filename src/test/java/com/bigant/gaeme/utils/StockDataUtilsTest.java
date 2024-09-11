9a8afd22aa4b518b54de7041f3e66dfdb835f717
tjdwns5063 <73011361+tjdwns5063@users.noreply.github.com>
2024-09-11 22:03:30 +0900

    fix: Disable 추가

--- src/test/java/com/bigant/gaeme/utils/StockDataUtilsTest.java
+++ src/test/java/com/bigant/gaeme/utils/StockDataUtilsTest.java
@@ -1,6 +1,7 @@
 package com.bigant.gaeme.utils;
 
 import com.bigant.gaeme.service.StockDataService;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ public class StockDataUtilsTest {
     private StockDataService stockDataService;
 
     @Test
+    @Disabled
     void insertStockDateInDatabase() {
         stockDataService.saveKrStockData();
         stockDataService.saveUsStockData();
