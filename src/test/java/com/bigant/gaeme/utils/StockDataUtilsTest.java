c4498ef75312c15b6e729a81e6ad5cb89e05383f
Seongjki <73011361+tjdwns5063@users.noreply.github.com>
2024-09-11 22:05:51 +0900

    feat: 주식 데이터 채워넣는 유틸 구현 (#51)

---
+++ src/test/java/com/bigant/gaeme/utils/StockDataUtilsTest.java
@@ -0,0 +1,22 @@
+package com.bigant.gaeme.utils;
+
+import com.bigant.gaeme.service.StockDataService;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StockDataUtilsTest {
+
+    @Autowired
+    private StockDataService stockDataService;
+
+    @Test
+    @Disabled
+    void insertStockDateInDatabase() {
+        stockDataService.saveKrStockData();
+        stockDataService.saveUsStockData();
+    }
+
+}
