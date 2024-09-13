b02697ba2fea7844de903c89ca8ca9ddafa37697
Seongjki <73011361+tjdwns5063@users.noreply.github.com>
2024-09-13 21:20:07 +0900

    feat: 게시글 조회 API 구현 (#52)

--- src/main/java/com/bigant/gaeme/GaemeApplication.java
+++ src/main/java/com/bigant/gaeme/GaemeApplication.java
@@ -2,8 +2,10 @@ package com.bigant.gaeme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GaemeApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/bigant/gaeme/repository/BoardTreePathRepository.java
+++ src/main/java/com/bigant/gaeme/repository/BoardTreePathRepository.java
@@ -2,7 +2,12 @@ package com.bigant.gaeme.repository;
 
 
 import com.bigant.gaeme.repository.entity.BoardTreePath;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardTreePathRepository extends JpaRepository<BoardTreePath, Long> {
+
+    Slice<BoardTreePath> findAllByAncestor_Id(Long boardId, Pageable pageable);
+
 }
