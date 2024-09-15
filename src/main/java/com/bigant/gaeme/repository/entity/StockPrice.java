6da141283d2248e765cdc1646b829684d8435528
tjdwns5063 <73011361+tjdwns5063@users.noreply.github.com>
2024-09-15 19:19:26 +0900

    feat: StockPrice 엔티티 및 ModelMapper Converter 구현

---
+++ src/main/java/com/bigant/gaeme/repository/entity/StockPrice.java
@@ -0,0 +1,42 @@
+package com.bigant.gaeme.repository.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class StockPrice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate businessDate;
+
+    private Long closePrice;
+
+    private Long openPrice;
+
+    private Long highestPrice;
+
+    private Long lowestPrice;
+
+    private Long accumulatedVolume;
+
+    private Long accumulatedTradingAmount;
+
+    private String previousDayContrastTodaySign;
+
+    private Long previousDayContrastTodayPrice;
+
+}
